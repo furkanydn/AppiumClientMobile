0e916b795e328e7fa9f839a4538d99c7a17d27ad
furkanaydin <furkan.aydin@inooster.com>
2022-04-07 14:24:49 +0300

    const added

---
+++ AppiumClientMobile/test/YuruBeIstanbul/Android/Objects/Constant/WalkthroughConst.cs
@@ -0,0 +1,13 @@
+namespace AppiumClientMobile.test.YuruBeIstanbul.Android.Objects.Constant
+{
+    public struct WalkthroughConst
+    {
+        public const string TcWalkthroughAppStarting = "Neler Yapabilirsin";
+        public const string TcWalkthroughStepText = "Adım At!";
+        public const string TcWalkthroughStepDesc = "Yürürken kendini fark et, sınırlarını zorla.";
+        public const string TcWalkthroughExploreText = "İstanbul'u Keşfet";
+        public const string TcWalkthroughExploreDesc = "Seçtiğin görev ve rotalar ile yaşadığın şehri adımla.";
+        public const string TcWalkthroughRewardsText = "Ödülleri Kazan";
+        public const string TcWalkthroughRewardsDesc = "Her adımında seni puan, rozet ve sürpriz ödüller bekliyor.";
+    }
+}
